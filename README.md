529544920f29eee6e796c7fe49ab51b28bc3a7c4
Matheus Almeida <matheusalmeidacosta1212@gmail.com>
2021-02-28 23:55:54 -0300

    new site and att readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Front-end sites
 
-<img src="image.png" alt="front-end projects">
+<img src="projects.png" alt="front-end projects">
 
 > Some projects using HTML, CSS and Javascript
 
-[Click here and check it out](https://almeida-matheus.github.io/front-end)
+<!-- [Click here and check it out](https://almeida-matheus.github.io/front-end) -->
+<h1>
+
+<h3 align="center">
+    <a href="https://almeida-matheus.github.io/front-end">See the project</a>
+<h3 >
---
+++ pergunte-rato/script.js
@@ -0,0 +1,58 @@
+/*===== CHANGE DIV VISIBILITY =====*/
+const btnQuestion = document.querySelector('.btn-question');
+const btnResult = document.querySelector('.btn-result');
+const divQuestion = document.querySelector('.div-question');
+const divResult = document.querySelector('.div-result');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const img = document.querySelector(".img-fluid");
+const textResult = document.querySelector(".text-result");
+
+btnQuestion.addEventListener('click', function () {
+
+    if (input1.value != "" && input2.value != "") {
+        divQuestion.classList.toggle('show-result');
+        divResult.classList.toggle('show-result');
+        img.setAttribute('src', "assets/shaman.png");
+
+        generateResults();
+    }
+
+})
+
+btnResult.addEventListener('click', function () {
+    divQuestion.classList.toggle('show-result');
+    divResult.classList.toggle('show-result');
+    img.setAttribute('src', "assets/mouse-reading.png");
+    //clear text
+    input1.value = "";
+    input2.value = "";
+})
+
+/*===== GENERATE RESULTS =====*/
+
+function generateResults() {
+    let textInput1 = input1.value;
+    let textInput2 = input2.value;
+
+    arrayText = [];
+    arrayText.push(textInput1);
+    arrayText.push(textInput2);
+
+    //Math.random()*1 = 0.1 ate 0.9
+    //Math.round = 0 > 0.5 >= 1
+    randomResult = arrayText[Math.round(Math.random() * 1)];
+
+    showResults(randomResult, textInput1, textInput2)
+}
+
+function showResults(randomResult, textInput1, textInput2) {
+
+    textResult.innerText = randomResult;
+
+    let strong1 = document.getElementById('strong1');
+    let strong2 = document.getElementById('strong2');
+
+    strong1.innerText = textInput1;
+    strong2.innerText = textInput2;
+}
