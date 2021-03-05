015ebbcc78dfe72eb0cd5b5b04a59dadc23cbb2b
Matheus Almeida <matheusalmeidacosta1212@gmail.com>
2021-03-04 22:41:26 -0300

    readme att and new site

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 <!-- [Click here and check it out](https://almeida-matheus.github.io/front-end) -->
 <h1>
 
-<h3 align="center">
+<!-- <h3 align="center">
     <a href="https://almeida-matheus.github.io/front-end">See the project</a>
-<h3 >
+<h3 > -->
+
+<h3 align="center">
+      <a href="https://almeida-matheus.github.io/front-end">
+        <img  src="https://img.shields.io/badge/-ACCESS%20THE%20PROJECT-1100FF?&style=for-the-badge&logoColor=fff"/>
+      </a>
+</h3>
---
+++ lyrics/script.js
@@ -0,0 +1,81 @@
+const formLetter = document.getElementById('formLetter');
+const inputSong = document.querySelector('#song');
+const inputArtist = document.querySelector('#artist');
+const buttonLetter = document.querySelector('#button');
+const lyricText = document.querySelector('#lyric');
+const divLyric = document.querySelector('lyricSong');
+
+//cors > permite q um site use recursos de outros sites msm em dominios diferentes
+function findLyrics(artist, song) {
+    //retorna uma promise
+    let promise = fetch(`https://api.lyrics.ovh/v1/${artist}/${song}`);
+    // let url = `https://api.lyrics.ovh/v1/${artist}/${song}`
+    // let promise = fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+    console.log(promise);
+    return promise;
+}
+
+formLetter.addEventListener('submit', el => {
+    //cancelar o padrão dele q é enviar, fazer o submit
+    el.preventDefault();
+    doSubmit();
+})
+
+//THEN
+// function doSubmit() {
+//     buttonLetter.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+
+//     //encodeURI: transforme space in %20 to get http
+//     let valueArtist = encodeURI(inputArtist.value.trim());
+//     let valueSong = encodeURI(inputSong.value.trim());
+//     if (valueArtist != "" || valueSong != "") {
+
+//         findLyrics(valueArtist, valueSong)
+//             .then(response => response.json())
+//             .then(data => {
+//                 console.log(data)
+//                 if (data.lyrics) {
+//                     document.getElementById("lyricSong").style.display = "block";
+//                     lyricText.innerHTML = data.lyrics;
+//                     buttonLetter.innerHTML = 'Enviar'
+//                 }
+//                 else {
+//                     lyricText.innerHTML = data.error;
+//                     buttonLetter.innerHTML = 'Enviar'
+//                 }
+//             })
+//             .catch(err => {
+//                 lyricText.innerHTML = `ops ${err}`;
+//                 buttonLetter.innerHTML = 'Enviar'
+//             })
+//     }
+//     buttonLetter.innerHTML = 'Enviar'
+// }
+
+// inputSong.value = ''
+// inputArtist.value = ''
+// inputSong.focus()
+
+// ASYNC AWAIT
+// async function doSubmit() {
+//     buttonLetter.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>'
+//     let valueArtist = encodeURI(inputArtist.value.trim());
+//     let valueSong = encodeURI(inputSong.value.trim());
+//     if (valueArtist != "" || valueSong != "") {
+//         try {
+//             const lyricResponse = await findLyrics(encodeURI(inputArtist.value.trim()), encodeURI(inputSong.value.trim()))
+//             const data = await lyricResponse.json()
+//             if (data.lyrics) {
+//                 lyricText.innerHTML = data.lyrics;
+//                 buttonLetter.innerHTML = 'Enviar'
+//             }
+//             else {
+//                 lyricText.innerHTML = data.error;
+//                 buttonLetter.innerHTML = 'Enviar'
+//             }
+//         }
+//         catch (err) {
+//             console.log(err)
+//         }
+//     }
+// }
